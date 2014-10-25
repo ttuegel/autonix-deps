39a88dd1753475935b5e871395ba5f49149a775c
Thomas Tuegel <ttuegel@gmail.com>
2014-10-25 09:12:08 -0500

    Deps: generalize type of generateDeps

--- src/Deps.hs
+++ src/Deps.hs
@@ -1,16 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Deps
        ( generateDeps
        , module Types
        ) where
 
+import Control.Monad.IO.Class
 import Control.Monad.State
 
 import Analyze
 import Generate
 import Types
 
-generateDeps :: [Analyzer (StateT Deps IO)] -> IO ()
+generateDeps :: MonadIO m => [Analyzer (StateT Deps m)] -> m ()
 generateDeps analyzers =
     analyzePackages (analyzeFiles analyzers) >>= writeDeps
