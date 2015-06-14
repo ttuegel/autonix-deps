fd211a68404b0eee673d2dbcee6bdec9a4153a24
Thomas Tuegel <ttuegel@gmail.com>
2015-06-14 18:45:09 -0500

    Revert "Add analyzeCMakeLists"
    This reverts commit ff34c95b759e94bb5154ea59eddc0cfc574a4cd0.

--- src/Autonix/CMake.hs
+++ src/Autonix/CMake.hs
@@ -2,8 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Autonix.CMake
-       ( analyzeCMakeLists
-       , analyzeCMakePackages
+       ( analyzeCMakePackages
        , analyzeCMakePrograms
        , cmakeAnalyzers
        , detectCMake
@@ -14,12 +13,10 @@ import Control.Monad.IO.Class
 import Control.Monad.State
 import Data.Conduit
 import qualified Data.Set as S
-import Data.Text (Text)
 import qualified Data.Text.Encoding as T
 import System.FilePath (takeFileName)
 
 import Autonix.Analyze
-import Autonix.Package (Package)
 import qualified Autonix.Package as Package
 import Autonix.Regex
 
@@ -28,23 +25,25 @@ detectCMake _ = awaitForever $ \(path, _) ->
     when (takeFileName path == "CMakeLists.txt")
         $ Package.nativeBuildInputs %= S.insert "cmake"
 
-analyzeCMakeLists :: MonadIO m => (Text -> Package -> Package) -> Analyzer m
-analyzeCMakeLists addDep _ = awaitForever $ \(path, contents) -> do
+analyzeCMakePackages :: MonadIO m => Analyzer m
+analyzeCMakePackages _ = awaitForever $ \(path, contents) -> do
     when (takeFileName path == "CMakeLists.txt") $ do
         let new = map T.decodeUtf8
                   $ concatMap (take 1 . drop 1)
                   $ match regex contents
             regex = makeRegex
                     "find_package[[:space:]]*\\([[:space:]]*([^[:space:],$\\)]+)"
-        mapM_ (modify . addDep) new
-
-analyzeCMakePackages :: MonadIO m => Analyzer m
-analyzeCMakePackages =
-  analyzeCMakeLists (\dep -> Package.buildInputs %~ S.insert dep)
+        Package.buildInputs %= S.union (S.fromList new)
 
 analyzeCMakePrograms :: MonadIO m => Analyzer m
-analyzeCMakePrograms =
-  analyzeCMakeLists (\dep -> Package.nativeBuildInputs %~ S.insert dep)
+analyzeCMakePrograms _ = awaitForever $ \(path, contents) -> do
+    when (takeFileName path == "CMakeLists.txt") $ do
+        let new = map T.decodeUtf8
+                  $ concatMap (take 1 . drop 1)
+                  $ match regex contents
+            regex = makeRegex
+                    "find_program[[:space:]]*\\([[:space:]]*([^[:space:],$\\)]+)"
+        Package.nativeBuildInputs %= S.union (S.fromList new)
 
 cmakeAnalyzers :: MonadIO m => [Analyzer m]
 cmakeAnalyzers = [detectCMake, analyzeCMakePackages, analyzeCMakePrograms]
