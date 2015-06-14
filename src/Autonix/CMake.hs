64dd9e3f37b08e3a8ebd256fe0b7dd95a5907cc2
Thomas Tuegel <ttuegel@gmail.com>
2015-06-14 18:53:15 -0500

    CMake: use lower-case package names

--- src/Autonix/CMake.hs
+++ src/Autonix/CMake.hs
@@ -13,6 +13,7 @@ import Control.Monad.IO.Class
 import Control.Monad.State
 import Data.Conduit
 import qualified Data.Set as S
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import System.FilePath (takeFileName)
 
@@ -28,22 +29,24 @@ detectCMake _ = awaitForever $ \(path, _) ->
 analyzeCMakePackages :: MonadIO m => Analyzer m
 analyzeCMakePackages _ = awaitForever $ \(path, contents) -> do
     when (takeFileName path == "CMakeLists.txt") $ do
-        let new = map T.decodeUtf8
+        let new = S.fromList
+                  $ map (T.toLower . T.decodeUtf8)
                   $ concatMap (take 1 . drop 1)
                   $ match regex contents
             regex = makeRegex
                     "find_package[[:space:]]*\\([[:space:]]*([^[:space:],$\\)]+)"
-        Package.buildInputs %= S.union (S.fromList new)
+        Package.buildInputs %= S.union new
 
 analyzeCMakePrograms :: MonadIO m => Analyzer m
 analyzeCMakePrograms _ = awaitForever $ \(path, contents) -> do
     when (takeFileName path == "CMakeLists.txt") $ do
-        let new = map T.decodeUtf8
+        let new = S.fromList
+                  $ map (T.toLower . T.decodeUtf8)
                   $ concatMap (take 1 . drop 1)
                   $ match regex contents
             regex = makeRegex
                     "find_program[[:space:]]*\\([[:space:]]*([^[:space:],$\\)]+)"
-        Package.nativeBuildInputs %= S.union (S.fromList new)
+        Package.nativeBuildInputs %= S.union new
 
 cmakeAnalyzers :: MonadIO m => [Analyzer m]
 cmakeAnalyzers = [detectCMake, analyzeCMakePackages, analyzeCMakePrograms]
